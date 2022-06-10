11069349bc963c7ca2627041bfe41eb7da00f64e
Aman <amankapil60@gmail.com>
2022-06-10 16:16:40 +0530

    crausal complete

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import os.path,sys
+CURRENT_DIR = os.path.dirname(__file__).replace('\\','/')
+PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, os.pardir))
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -120,5 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
